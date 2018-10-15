5776e5440f03cf37d20a47633f2af9eb1595f036
ZSS\zss <hpu_zss@163.com>
2018-10-15 18:13:47 +0800

    初始建立

---
+++ test_data/demo.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import os
+import glob
+import time
+
+if __name__ == '__main__':
+    start_time = time.clock()
+    print("hello")
+    end_time = time.clock()
+
+    print("time: %.4f secs." % (end_time - start_time))
+
+
