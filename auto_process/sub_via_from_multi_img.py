33eac34ef9c6bd4bf5e453250d01447ac3a8f56a
hpuzss <hpu1203zss@gmail.com>
2019-05-30 15:52:29 +0800

    新增用于从多个有重叠区域的影像上进行样本的裁剪,要求shp有图像对应影像信息的记录

---
+++ auto_process/sub_via_from_multi_img.py
@@ -0,0 +1,246 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+"""
+# @Time    : 2019/2/15 16:16
+# @Author  : zhaoss
+# @FileName: subset_sample_shapefile.py
+# @Email   : zhaoshaoshuai@hnnydsj.com
+Description:
+    对栅格分要素裁剪
+Parameters
+    参数1：输入影像
+    参数2：用于裁剪的矢量
+    参数3：裁剪结果输出路径
+"""
+
+import os
+import sys
+import glob
+import numpy as np
+import time
+import fnmatch
+from osgeo import gdal, ogr, osr, gdalconst
+
+try:
+    progress = gdal.TermProgress_nocb
+except:
+    progress = gdal.TermProgress
+
+
+def rpj_vec(lyr, srs):
+    """对矢量进行投影变换"""
+    # 创建临时矢量文件
+    mem_dri = ogr.GetDriverByName('Memory')
+    mem_ds = mem_dri.CreateDataSource(' ')
+    outLayer = mem_ds.CreateLayer(' ', geom_type=lyr.GetGeomType(), srs=srs)
+    # 附加字段
+    outLayer.CreateFields(lyr.schema)
+    # 逐要素进行投影转换
+    out_feat = ogr.Feature(outLayer.GetLayerDefn())
+    for in_feat in lyr:
+        geom = in_feat.geometry().Clone()
+        geom.TransformTo(srs)
+        out_feat.SetGeometry(geom)
+        # 写入属性信息
+        for i in range(in_feat.GetFieldCount()):
+            out_feat.SetField(i, in_feat.GetField(i))
+        outLayer.CreateFeature(out_feat)
+    return mem_ds, outLayer
+
+
+def searchfiles(dirpath, partfileinfo='*', recursive=False):
+    """列出符合条件的文件（包含路径），默认不进行递归查询，当recursive为True时同时查询子文件夹"""
+    # 定义结果输出列表
+    filelist = []
+    # 列出根目录下包含文件夹在内的所有文件目录
+    pathlist = glob.glob(os.path.join(os.path.sep, dirpath, "*"))
+    # 逐文件进行判断
+    for mpath in pathlist:
+        if os.path.isdir(mpath):
+            # 默认不判断子文件夹
+            if recursive:
+                filelist += searchfiles(mpath, partfileinfo, recursive)
+        elif fnmatch.fnmatch(os.path.basename(mpath), partfileinfo):
+            filelist.append(mpath)
+        # 如果mpath为子文件夹，则进行递归调用，判断子文件夹下的文件
+
+    return filelist
+
+
+def Feature_memory_shp(feat, sr):
+    """将指定的geometry导出为内存中单独的shpfile"""
+    fid = feat.GetFID()
+    # 在内存中创建临时的矢量文件，用以存储单独的要素
+    # 创建临时矢量文件
+    mem_dri = ogr.GetDriverByName('Memory')
+    mem_ds = mem_dri.CreateDataSource(' ')
+    outLayer = mem_ds.CreateLayer(' ', geom_type=ogr.wkbPolygon, srs=sr)
+    # 给图层中创建字段用以标识原来的FID
+    coor_fld = ogr.FieldDefn('ID_FID', ogr.OFTInteger)
+    outLayer.CreateField(coor_fld)
+    # 创建虚拟要素，用以填充原始要素
+    out_defn = outLayer.GetLayerDefn()
+    out_feat = ogr.Feature(out_defn)
+    # 对ID_FID字段填充值
+    fld_index = outLayer.GetLayerDefn().GetFieldIndex('ID_FID')
+    out_feat.SetField(fld_index, fid)
+    # 填充要素
+    out_feat.SetGeometry(feat.geometry())
+    outLayer.CreateFeature(out_feat)
+    return mem_ds, outLayer
+
+
+def shp2raster(raster_ds, shp_layer, ext):
+    # 将行列整数浮点化
+    ext = np.array(ext) * 1.0
+    # 获取栅格数据的基本信息
+    raster_prj = raster_ds.GetProjection()
+    raster_geo = raster_ds.GetGeoTransform()
+    # 根据最小重叠矩形的范围进行矢量栅格化
+    ulx, uly = gdal.ApplyGeoTransform(raster_geo, ext[0], ext[1])
+    x_size = ext[2] - ext[0]
+    y_size = ext[3] - ext[1]
+    # 创建mask
+    # out = r"F:\test_data\clipraster\gdal_mask2\test3.tif"
+    # mask_ds = gdal.GetDriverByName('GTiff').Create(out, int(x_size), int(y_size), 1, gdal.GDT_Byte)
+    mask_ds = gdal.GetDriverByName('MEM').Create('', int(x_size), int(y_size), 1, gdal.GDT_Byte)
+    mask_ds.SetProjection(raster_prj)
+    mask_geo = [ulx, raster_geo[1], 0, uly, 0, raster_geo[5]]
+    mask_ds.SetGeoTransform(mask_geo)
+    # 矢量栅格化
+    gdal.RasterizeLayer(mask_ds, [1], shp_layer, burn_values=[1])
+    return mask_ds
+
+
+def min_rect(raster_ds, shp_layer):
+    # 获取栅格的大小
+    x_size = raster_ds.RasterXSize
+    y_size = raster_ds.RasterYSize
+    # 获取是矢量的范围
+    extent = shp_layer.GetExtent()
+    # 获取栅格的放射变换参数
+    raster_geo = raster_ds.GetGeoTransform()
+    # 计算逆放射变换系数
+    raster_inv_geo = gdal.InvGeoTransform(raster_geo)
+    # 计算在raster上的行列号
+    # 左上
+    off_ulx, off_uly = map(round, gdal.ApplyGeoTransform(raster_inv_geo, extent[0], extent[3]))
+    # 右下
+    off_drx, off_dry = map(round, gdal.ApplyGeoTransform(raster_inv_geo, extent[1], extent[2]))
+    # 判断是否有重叠区域
+    if off_ulx >= x_size or off_uly >= y_size or off_drx <= 0 or off_dry <= 0:
+        return 0
+    # 限定重叠范围在栅格影像上
+    # 列
+    offset_column = np.array([off_ulx, off_drx])
+    offset_column = np.maximum((np.minimum(offset_column, x_size - 1)), 0)
+    # 行
+    offset_line = np.array([off_uly, off_dry])
+    offset_line = np.maximum((np.minimum(offset_line, y_size - 1)), 0)
+
+    return [offset_column[0], offset_line[0], offset_column[1], offset_line[1]]
+
+
+def mask_raster(raster_ds, mask_ds, outfile, ext):
+    # 将行列整数浮点化
+    ext = np.array(ext) * 1.0
+    # 获取栅格数据的基本信息
+    raster_prj = raster_ds.GetProjection()
+    raster_geo = raster_ds.GetGeoTransform()
+    bandCount = raster_ds.RasterCount
+    dataType = raster_ds.GetRasterBand(1).DataType
+    # 根据最小重叠矩形的范围进行矢量栅格化
+    ulx, uly = gdal.ApplyGeoTransform(raster_geo, ext[0], ext[1])
+    x_size = ext[2] - ext[0]
+    y_size = ext[3] - ext[1]
+    # 创建输出影像
+    result_ds = gdal.GetDriverByName('GTiff').Create(outfile, int(x_size), int(y_size), bandCount, dataType)
+    result_ds.SetProjection(raster_prj)
+    result_geo = [ulx, raster_geo[1], 0, uly, 0, raster_geo[5]]
+    result_ds.SetGeoTransform(result_geo)
+    # 获取掩模
+    mask = mask_ds.GetRasterBand(1).ReadAsArray()
+    mask = 1 - mask
+    # 对原始影像进行掩模并输出
+    for band in range(bandCount):
+        banddata = raster_ds.GetRasterBand(band + 1).ReadAsArray(int(ext[0]), int(ext[1]), int(x_size), int(y_size))
+        banddata = np.choose(mask, (banddata, 0))
+        result_ds.GetRasterBand(band + 1).WriteArray(banddata)
+    return 1
+
+
+def main(raster_files, shp, out_dir, fieldName='name'):
+    # 获取一个影像
+    # 打开矢量
+    shp_ds = ogr.Open(shp)
+    shp_lyr = shp_ds.GetLayer()
+    shp_sr = shp_lyr.GetSpatialRef()
+    # 拆分矢量用以对单个要素进行裁剪
+    # 定义变量用以显示进度条
+    count = 0
+    num_feature = shp_lyr.GetFeatureCount()
+    for feat in shp_lyr:
+        # 要素提取为图层
+        feat_ds, feat_lyr = Feature_memory_shp(feat, shp_sr)
+        # 获取该要素的品类名称
+        category_name = feat.GetField(fieldName).split("-")
+        # 获取要素的image属性值用以确定和该要素相关的影像
+        relate_image_str = feat.GetField("image")
+        relate_images = relate_image_str.split("/")
+        images_path = []
+        for image_str in relate_images:
+            # 在影像文件加下获取该影响的绝对路径
+            id = image_str.split("_")[-1]
+            img_path = searchfiles(raster_files, partfileinfo="*" + id + "*.tiff")
+            if len(img_path) == 0:
+                print("the image dose not exist:{0}, the relevant vector is :{1}".format(image_str,
+                                                                                         category_name[0] + "_" +
+                                                                                         category_name[1]))
+                continue
+            else:
+                images_path.append(img_path[0])
+        # 针对该要素进行裁剪
+        for image in images_path:
+            # 获取和该要素相关的影像名称
+            img_name = os.path.splitext(os.path.basename(image))[0].split("_")
+            # 拼接样本的输出名字
+            sample_out_name = '_'.join([category_name[0], img_name[0], img_name[4], img_name[5], category_name[1]])
+            # 拼接输出路径
+            outpath = os.path.join(out_dir, sample_out_name + '.tiff')
+            # 打开影像开始裁剪
+            raster_ds = gdal.Open(image)
+            # 要素裁剪
+            # 计算矢量和栅格的最小重叠矩形
+            offset = min_rect(raster_ds, feat_lyr)
+            # 判断是否有重叠区域，如无（0），则跳过
+            if offset == 0:
+                continue
+            # 矢量栅格化
+            mask_ds = shp2raster(raster_ds, feat_lyr, offset)
+            # 进行裁剪
+            res = mask_raster(raster_ds, mask_ds, outpath, offset)
+            raster_ds = None
+        progress((count + 1) / num_feature)
+        count += 1
+    shp_ds = None
+    return None
+
+
+if __name__ == '__main__':
+    # 支持中文路径
+    gdal.SetConfigOption("GDAL_FILENAME_IS_UTF8", "NO")
+    # 支持中文属性字段
+    gdal.SetConfigOption("SHAPE_ENCODING", "GBK")
+    # 注册所有ogr驱动
+    ogr.RegisterAll()
+    # 注册所有gdal驱动
+    gdal.AllRegister()
+
+    start_time = time.clock()
+    in_dir = r"G:\Fusion"
+    shpfile = r"G:\sample_shp\zong.shp"
+    out_dir = r"H:\sample_out"
+    main(in_dir, shpfile, out_dir)
+    end_time = time.clock()
+
+    print("time: %.4f secs." % (end_time - start_time))
