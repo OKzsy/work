173a7bc62c20d77c16a4577affefb9d32e2e6a93
ZSS\zss <hpu_zss@163.com>
2019-06-12 20:23:29 +0800

    用以测试numexpr

--- test_code/test1.py
+++ test_code/test1.py
@@ -1,4 +1,9 @@
-from functools import partial
+import numpy as np
+import numexpr as ne
 
-def f(a, b, c):
-    print("{} {} {}".format(a, b, c))
+arr = np.array([-1, 2, 3, np.nan])
+bar = np.array([-1, 5, 3, np.nan])
+phi = 1
+# index = np.where(np.isnan(arr))
+var = ne.evaluate("(bar - phi) ** 2")
+print(var)
