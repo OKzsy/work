e57e95ea4f2fd50ea37990c8ffd0f04a3a54898c
ZSS\zss <hpu_zss@163.com>
2020-04-27 17:48:59 +0800

    实现cart决策树分类

--- random_forest/gini.py
+++ random_forest/gini.py
@@ -75,7 +75,7 @@ def build_tree(data):
     if data.shape[0] == 0:
         return node()
     # 计算当前的gini指数
-    currentgini = cal_gini_index(data[:, 0])
+    currentgini = cal_gini_index(data[:, -1])
     bestgini = 0.0
     # 最佳切分属性以及最佳切分点
     bestcriteria = None
@@ -84,14 +84,67 @@ def build_tree(data):
     # 样本中的可用特征个数
     feature_num = data.shape[1] - 1
     # 寻找最好的切分属性和切分点
-    
-    return 1
+    for fea in range(feature_num):
+        # 获取所在fea特征所有可能取得的值
+        tmp_feature = data[:, fea]
+        feature_values = np.unique(tmp_feature)
+        # 对每一个可能的值进行数据集划分，并计算gini指数
+        for value in feature_values:
+            # 根据fea特征中的值将数据集划分为左右子树
+            index1 = np.where(tmp_feature >= value)
+            size1 = index1[0].size
+            set1 = data[index1[0], :]
+            index2 = np.where(tmp_feature < value)
+            size2 = index2[0].size
+            set2 = data[index2[0], :]
+            # 计算拆分后的gini指数
+            nowgini = (size1 * cal_gini_index(set1[:, -1]) + size2 * cal_gini_index(set2[:, -1])) / data.shape[0]
+            # 计算gini指数增加量
+            gain = currentgini - nowgini
+            # 判断此划分是否比当前划分更好
+            if gain > bestgini and size1 > 0 and size2 > 0:
+                bestgini = gain
+                bestcriteria = (fea, value)
+                bestsets = (set1, set2)
+    # 判断划分是否结束
+    if bestgini > 0:
+        right = build_tree(bestsets[0])
+        left = build_tree(bestsets[1])
+        return node(fea=bestcriteria[0], value=bestcriteria[1], right=right, left=left)
+    else:
+        # 返回当前的类别标签作为最终的类别标签
+        return node(results=np.unique(data, return_counts=True))
+
+
+def predict(sample, tree):
+    """
+    对每一个样本进行预测
+    :param sample: numpy array 需要预测的样本
+    :param tree: 构建好的决策树
+    :return: 所属类别
+    """
+    # 只有树根
+    if tree.results != None:
+        return tree.results[0][0]
+    else:
+        # 有左右子树
+        val_sample = sample[tree.fea]
+        branch = None
+        if val_sample >= tree.value:
+            branch = tree.right
+        else:
+            branch = tree.left
+        return predict(sample, branch)
 
 
 def main():
-    ls = [[1, 1, 1], [1, 1, 1], [0, 1, 0], [0, 0, 1], [0, 0, 1]]
+    ls = [[1, 1, 1], [1, 1, 1], [1, 0, 0], [0, 1, 0], [0, 1, 0]]
     label_ar = np.array(ls)
-    res = cal_gini_index(label_ar[:, 0])
+    res = cal_gini_index(label_ar[:, -1])
+    print(res)
+    tree = build_tree(label_ar)
+    sample = np.array([0, 1])
+    res = predict(sample, tree)
     print(res)
     return None
 
