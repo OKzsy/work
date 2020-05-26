0d603c4c266cc0256c8b83d6f1ec6e845da05f3b
ZSS\zss <hpu_zss@163.com>
2020-05-26 14:03:18 +0800

    测试程序

--- random_forest/test.py
+++ random_forest/test.py
@@ -1,49 +1,17 @@
-#!/usr/bin/env python3
-# -*- coding:utf-8 -*-
-"""
-# @Time    : 2020/5/25 14:49
-# @Author  : zhaoss
-# @FileName: test.py
-# @Email   : zhaoshaoshuai@hnnydsj.com
-Description:
+import multiprocessing
 
 
-Parameters
+def job(x, y):
+    return x * y
 
 
-"""
-
-import os
-import sys
-import glob
-import time
-import fnmatch
-import numpy as np
-from osgeo import gdal, ogr, osr, gdalconst
-
-try:
-    progress = gdal.TermProgress_nocb
-except:
-    progress = gdal.TermProgress
-
-
-def main():
-    return None
-
-
-if __name__ == '__main__':
-    # 支持中文路径
-    gdal.SetConfigOption("GDAL_FILENAME_IS_UTF8", "YES")
-    # 支持中文属性字段
-    gdal.SetConfigOption("SHAPE_ENCODING", "GBK")
-    # 注册所有ogr驱动
-    ogr.RegisterAll()
-    # 注册所有gdal驱动
-    gdal.AllRegister()
-    start_time = time.time()
-
-    main()
-    end_time = time.time()
-    print("time: %.4f secs." % (end_time - start_time))
-
-
+if __name__ == "__main__":
+    pool = multiprocessing.Pool(processes=2)
+    res = []
+    for i in range(3):
+        a = i
+        b = i + 10
+        res.append(pool.apply_async(job, (a, b)))
+    pool.close()
+    pool.join()
+    print([r.get() for r in res])
