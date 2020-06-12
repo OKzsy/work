3c4a6cfb339c315799a70b78a5ded7a8e1da735a
ZSS\zss <hpu_zss@163.com>
2020-06-12 16:03:42 +0800

    改变增加指数的结构，实现自动添加波段

--- random_forest/add_veg_index.py
+++ random_forest/add_veg_index.py
@@ -27,12 +27,37 @@ except:
     progress = gdal.TermProgress
 
 
-def add_ndvi(red=None, inf=None):
+def add_mtci(blue=None, green=None, red=None, red_edge1=None, red_edge2=None, red_edge3=None, inf=None):
+    """
+    针对含有红边波段的哨兵数据添加mtci指数
+    :param blue:
+    :param green:
+    :param red:
+    :param red_edge1:
+    :param red_edge2:
+    :param red_edge3:
+    :param inf:
+    :return:
+    """
+    red_edge2 = red_edge2.astype(np.int16)
+    red_edge1 = red_edge1.astype(np.int16)
+    red = red.astype(np.int16)
+    mtci = (((red_edge2 - red_edge1) / (red_edge1 - red + 0.000001)) * 1000).astype(np.int16)
+    red_edge2 = red_edge1 = red = None
+    return mtci
+
+
+def add_ndvi(blue=None, green=None, red=None, red_edge1=None, red_edge2=None, red_edge3=None, inf=None):
     """
     增加归一化植被指数
-    :param red: 红波段
-    :param inf: 近红外波段
-    :return: 归一化植被指数
+    :param blue:
+    :param green:
+    :param red:
+    :param red_edge1:
+    :param red_edge2:
+    :param red_edge3:
+    :param inf:
+    :return:
     """
     red = red.astype(np.int16)
     inf = inf.astype(np.int16)
@@ -50,25 +75,40 @@ def main(infile, outfile):
     bandnum = in_ds.RasterCount
     # 获取数据
     oridata = in_ds.ReadAsArray()
-    # 增加各种指数
-    new_band = 0
-    # 归一化植被指数
-    ndvi = add_ndvi(red=oridata[2, :, :], inf=oridata[6, :, :])
-    new_band += 1
-    # 合并所有增加的指数
-    index_arr= np.zeros((new_band, ysize, xsize))
-    index_arr[0, :, :] = ndvi
-    ndvi = None
+    index_list = [add_ndvi, add_mtci]
+    index_num = len(index_list)
+    index_arr = np.zeros((index_num, ysize, xsize))
+    for ifunc in range(index_num):
+        func = index_list[ifunc]
+        if bandnum == 4:
+            index_arr[ifunc, :, :] = func(blue=oridata[0, :, :],
+                                          green=oridata[1, :, :],
+                                          red=oridata[2, :, :],
+                                          inf=oridata[3, :, :])
+        else:
+            index_arr[ifunc, :, :] = func(blue=oridata[0, :, :],
+                                          green=oridata[1, :, :],
+                                          red=oridata[2, :, :],
+                                          red_edge1=oridata[3, :, :],
+                                          red_edge2=oridata[4, :, :],
+                                          red_edge3=oridata[5, :, :],
+                                          inf=oridata[6, :, :])
     # 写出数据
     tiff_driver = gdal.GetDriverByName('GTiff')
-    out_ds = tiff_driver.Create(outfile, xsize, ysize, bandnum + new_band, gdal.GDT_Int16)
+    all_band = bandnum + index_num
+    out_ds = tiff_driver.Create(outfile, xsize, ysize, all_band, gdal.GDT_Int16)
     out_ds.SetProjection(rpj)
     out_ds.SetGeoTransform(geo)
+    countband = 1
     # 写原始影像
     for iband in range(bandnum):
         out_ds.GetRasterBand(iband + 1).WriteArray(oridata[iband, :, :])
-    for iband in range(new_band):
+        progress(countband / all_band)
+        countband += 1
+    for iband in range(index_num):
         out_ds.GetRasterBand(bandnum + iband + 1).WriteArray(index_arr[iband, :, :])
+        progress(countband / all_band)
+        countband += 1
     out_ds.FlushCache()
     out_ds = None
 
@@ -85,8 +125,8 @@ if __name__ == '__main__':
     # 注册所有gdal驱动
     gdal.AllRegister()
     start_time = time.time()
-    infile = r"F:\test_data\dengfeng\S2\L2A_20200318_dengfeng.tif"
-    outfile = r"F:\test_data\dengfeng\S2\L2A_20200318_dengfeng_with_veg_index.tif"
+    infile = r"\\192.168.0.234\nydsj\user\ZSS\dengfeng\S2\L2A_20200318_dengfeng.tif"
+    outfile = r"\\192.168.0.234\nydsj\user\ZSS\dengfeng\S2\L2A_20200318_dengfeng_index.tif"
     main(infile, outfile)
     end_time = time.time()
     print("time: %.4f secs." % (end_time - start_time))
