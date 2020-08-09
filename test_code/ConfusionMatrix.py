a9421b14639d5b1e72db3b1cff22aa2aebf7f8f6
hpuzss <hpu1203zss@gmail.com>
2020-08-09 19:56:11 +0800

    基于混淆矩阵，增加标签点F1-Score参数的计算，增加Kappa系数的计算

--- test_code/ConfusionMatrix.py
+++ test_code/ConfusionMatrix.py
@@ -6,6 +6,8 @@ import numpy as np
 Matrix = [[10, 1, 2], [3, 15, 4], [5, 6, 20]]
 mat = np.array(Matrix)
 flags = ['猫', '狗', '猪']
+micro_ppv = np.zeros(len(flags))
+micro_trp = np.zeros(len(flags))
 for iflag in range(len(flags)):
     Tp = mat[iflag, iflag]
     Fp = np.sum(mat[iflag, :]) - Tp
@@ -14,7 +16,9 @@ for iflag in range(len(flags)):
 
     acc = np.trace(mat) / (Tp + Fp + Fn + Tn)
     ppv = Tp / (Tp + Fp)
+    micro_ppv[iflag] = ppv
     trp = Tp / (Tp + Fn)
+    micro_trp[iflag] = trp
     tnr = Tn / (Fp + Tn)
     F1Score = 2 * ppv * trp / (ppv + trp)
     print('The flag is: {}'.format(flags[iflag]))
@@ -23,3 +27,15 @@ for iflag in range(len(flags)):
     print('The Sensitivity is: {}'.format(trp))
     print('The Specificity is: {}'.format(tnr))
     print('The F1-Score is: {}'.format(F1Score))
+micro_p = np.average(micro_ppv)
+micro_t = np.average(micro_trp)
+micro_f1 = 2 * micro_p * micro_t / (micro_p + micro_t)
+print('The Micro-F1-Score is: {}'.format(micro_f1))
+# 计算kappa系数
+pe_rows = np.sum(mat, axis=0)
+pe_cols = np.sum(mat, axis=1)
+sum_total = sum(pe_cols)
+pe = np.dot(pe_rows, pe_cols) / float(sum_total ** 2)
+po = np.trace(mat) / float(sum_total)
+kappa = (po - pe) / (1 - pe)
+print('The Kappa is: {:<5.4}'.format(kappa))
