4c7dd766c96bae5a90c7349c7a4aa660b7bb026e
ZSS\zss <hpu_zss@163.com>
2020-10-09 15:30:47 +0800

    创建定量反演工程

---
+++ Inversion/Inv_lai.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+"""
+# @Time    : 2020/10/9 15:29
+# @Author  : zhaoss
+# @FileName: Inv_lai.py
+# @Email   : zhaoshaoshuai@hnnydsj.com
+Description:
+
+
+Parameters
+
+
+"""
+
+import os
+import sys
+import glob
+import time
+import fnmatch
+import numpy as np
+from osgeo import gdal, ogr, osr, gdalconst
+
+try:
+    progress = gdal.TermProgress_nocb
+except:
+    progress = gdal.TermProgress
+
+
+def main():
+    return None
+
+
+if __name__ == '__main__':
+    # 支持中文路径
+    gdal.SetConfigOption("GDAL_FILENAME_IS_UTF8", "YES")
+    # 支持中文属性字段
+    gdal.SetConfigOption("SHAPE_ENCODING", "GBK")
+    # 注册所有ogr驱动
+    ogr.RegisterAll()
+    # 注册所有gdal驱动
+    gdal.AllRegister()
+    start_time = time.time()
+
+    main()
+    end_time = time.time()
+    print("time: %.4f secs." % (end_time - start_time))
+
+
