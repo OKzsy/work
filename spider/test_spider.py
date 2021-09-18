bb8046e67449bc4d2db9598458714a9c20d5998d
OKzsy <hpu_zss@163.com>
2021-09-18 15:56:27 +0800

    修复部分bug

--- spider/test_spider.py
+++ spider/test_spider.py
@@ -18,7 +18,7 @@ def main(url):
     chrome_options.add_argument(r'--user-data-dir=C:\Users\01\AppData\Local\Google\Chrome\User Data\Default')
     # 获取浏览器驱动
     driver = webdriver.Chrome(chrome_options=chrome_options)
-    # driver.maximize_window()
+    driver.maximize_window()
     driver.get(url)
     # 登录账号
     if driver.find_element_by_xpath('//*[@id="top-index-loginUrl"]').text:
@@ -41,7 +41,7 @@ def main(url):
     # 等待页加载，并切换到需要购买的产品页面等待抢购
     page_wait = WebDriverWait(driver, 10)
     # element_pro_list = page_wait.until(lambda d: d.find_element_by_xpath('//*[@id="pro-list"]/li[8]/div/a'))
-    element_pro_list = page_wait.until(lambda d: d.find_element_by_xpath('//*[@id="pro-list"]/li[4]/div/a'))
+    element_pro_list = page_wait.until(lambda d: d.find_element_by_xpath('//*[@id="pro-list"]/li[1]'))
     element_pro_list.click()
     # 设置等待
     wait = WebDriverWait(driver, 10)
@@ -58,9 +58,9 @@ def main(url):
     # 等待商品可以抢购，并抢购
     # 设置等待
     def buyisable(drv):
-        # element_product = drv.find_element_by_xpath('//*[@id="pro-operation"]/a')
-        element_product = drv.find_element_by_xpath('//*[@id="pro-operation"]/span')
+        element_product = drv.find_element_by_xpath('//*[@id="pro-operation"]/*[contains(@class,"product-button02")]')
         if 'disabled' in element_product.get_attribute('class'):
+            print('check')
             return False
         else:
             return element_product
@@ -72,21 +72,34 @@ def main(url):
             element_buy = buy_wait.until(lambda d: buyisable(d))
             print(element_buy.text)
             element_buy.click()
-            ele_frame = WebDriverWait(driver, 2).until(lambda d: d.find_element_by_css_selector('#queueIframe'))
-            driver.switch_to.frame(ele_frame)
-            active = True
-            while active:
-                queue_ele = driver.find_element_by_css_selector('body > div.queue-tips')
-                print(queue_ele.text)
-                if '抱歉' in queue_ele.text:
-                    driver.find_element_by_css_selector('body > div.queue-btn').click()
-                    driver.switch_to.default_content()
-                    active = False
-    except:
+            try:
+                ele_frame = WebDriverWait(driver, 2).until(lambda d: d.find_element_by_css_selector('#queueIframe'))
+            except:
+                buy_active = False
+                break
+            else:
+                driver.switch_to.frame(ele_frame)
+                active = True
+                while active:
+                    queue_ele = driver.find_element_by_css_selector('body > div.queue-tips')
+                    if len(queue_ele) == 0:
+                        buy_active = False
+                        break
+                    else:
+                        # print(EC.presence_of_element_located((By.CSS_SELECTOR, 'body > div.queue-tips'))(driver))
+                        # queue_ele = WebDriverWait(driver, 2).until(
+                        #     EC.presence_of_element_located((By.CSS_SELECTOR, 'body > div.queue-tips')))
+                        print(queue_ele.text)
+                        if '抱歉' in queue_ele.text:
+                            driver.find_element_by_css_selector('body > div.queue-btn').click()
+                            driver.switch_to.default_content()
+                            active = False
+    except Exception as e:
+        print(e)
         driver.quit()
     os.system('pause')
 
 
 if __name__ == '__main__':
-    url = 'https://www.vmall.com/list-277'
+    url = 'https://www.vmall.com/list-56'
     main(url)
